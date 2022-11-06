cd259c77a1ae22b50036d314560267cabc7d8a0b
David Santiago Velasco <david.velasco@celuweb.com>
2022-11-06 16:53:49 -0500

    app funcional refactorizada del video

---
+++ lib/SelectBonedDevicePage.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:fundamento/BluetoothDeviceListEntry.dart';
+
+class SelectBondedDevicePage extends StatefulWidget {
+  /// If true, on page start there is performed discovery upon the bonded devices.
+  /// Then, if they are not avaliable, they would be disabled from the selection.
+  final bool checkAvailability;
+  final Function onCahtPage;
+
+  const SelectBondedDevicePage(
+      {this.checkAvailability = true, required this.onCahtPage});
+
+  @override
+  _SelectBondedDevicePage createState() => _SelectBondedDevicePage();
+}
+
+enum _DeviceAvailability {
+  no,
+  maybe,
+  yes,
+}
+
+class _DeviceWithAvailability extends BluetoothDevice {
+  BluetoothDevice device;
+  _DeviceAvailability availability;
+  int rssi;
+
+  _DeviceWithAvailability(this.device, this.availability, this.rssi)
+      : super(address: 'android');
+}
+
+class _SelectBondedDevicePage extends State<SelectBondedDevicePage> {
+  List<_DeviceWithAvailability> devices = <_DeviceWithAvailability>[];
+
+  // Availability
+  StreamSubscription<BluetoothDiscoveryResult>? _discoveryStreamSubscription;
+  bool? _isDiscovering;
+
+  _SelectBondedDevicePage();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _isDiscovering = widget.checkAvailability;
+
+    if (_isDiscovering!) {
+      _startDiscovery();
+    }
+    // Setup a list of the bonded devices
+    FlutterBluetoothSerial.instance
+        .getBondedDevices()
+        .then((List<BluetoothDevice> bondedDevices) {
+      setState(() {
+        devices = bondedDevices
+            .map(
+              (device) => _DeviceWithAvailability(
+                  device,
+                  widget.checkAvailability
+                      ? _DeviceAvailability.maybe
+                      : _DeviceAvailability.yes,
+                  1),
+            )
+            .toList();
+      });
+    });
+  }
+
+  void _restartDiscovery() {
+    setState(() {
+      _isDiscovering = true;
+    });
+
+    _startDiscovery();
+  }
+
+  void _startDiscovery() {
+    _discoveryStreamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+      setState(() {
+        Iterator i = devices.iterator;
+        while (i.moveNext()) {
+          var device = i.current;
+          if (device.device == r.device) {
+            device.availability = _DeviceAvailability.yes;
+            device.rssi = r.rssi;
+          }
+        }
+      });
+    });
+
+    _discoveryStreamSubscription!.onDone(() {
+      setState(() {
+        _isDiscovering = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and cancel discovery
+    _discoveryStreamSubscription!.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<BluetoothDeviceListEntry> list = devices
+        .map(
+          (device) => BluetoothDeviceListEntry(
+            device: device.device,
+            // rssi: _device.rssi,
+            // enabled: _device.availability == _DeviceAvailability.yes,
+            onTap: () {
+              widget.onCahtPage(device.device);
+            },
+          ),
+        )
+        .toList();
+    return ListView(
+      children: list,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:fundamento/routes/routes.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:fundamento/SelectBonedDevicePage.dart';
+import 'package:fundamento/chat_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      //tema dark
-      theme: ThemeData.dark(),
-      title: 'proyecto bluetooth',
-      //las rutas son instanciadas de la carpeta routes y se llama el mapa de "appRoutes"
-      routes: appRoutes,
-      //se inicializa con el nombre de la ruta creada en appRoutes
-      initialRoute: 'home',
+      theme: ThemeData.dark().copyWith(
+          appBarTheme: const AppBarTheme(color: Colors.indigo, elevation: 0)),
+      home: FutureBuilder(
+        future: FlutterBluetoothSerial.instance.requestEnable(),
+        builder: (context, future) {
+          if (future.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: SizedBox(
+                height: double.infinity,
+                child: Center(
+                  child: Icon(
+                    Icons.bluetooth_disabled,
+                    size: 200.0,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return Home();
+          }
+        },
+        // child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Connection'),
+      ),
+      body: SelectBondedDevicePage(
+        onCahtPage: (device1) {
+          BluetoothDevice device = device1;
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return ChatPage(server: device);
+              },
+            ),
+          );
+        },
+      ),
+    ));
   }
 }
